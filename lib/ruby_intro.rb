d795daf043699824b57b0352eb4279ce6f55b6cd
Zawahir Faheem <zawahirfaheem@tamu.edu>
2018-02-02 22:00:21 -0600

    Final commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,69 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0){|sum, el| sum + el}
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+	if(arr.empty?)
+	return 0
+    else
+	arr.max(2).reduce(:+)
+	end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+	if(arr.empty?)
+	return false
+	else
+	(arr.empty? && n.zero?) || arr.permutation(2).any? { |a, b| a + b == n }
+	end
 end
+
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+   "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
-end
+  !!(s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
+end 
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+	if (s) == "0"
+    return true
+	end
+	if /^[01]*(00)$/.match(s) #|| /^0$/.match(s)
+		return true
+	else
+		return false
+	
+	
+	end
 end
-
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+	
+	def initialize(isbn,price)
+		
+		if(isbn=="" || price<=0)
+		raise ArgumentError.new("Incorrect isbn or price")
+		end
+		@isbn = isbn
+		@price = price
+	end
+	attr_accessor :isbn, :price
+
+	def getter
+	@isbn
+	@price
+	end
+	
+	def price_as_string
+	@price= '$' + '%.2f' % @price
+	@price
+	end
 end
